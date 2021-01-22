e18db357e5023e140fda560893027404cefa2b7b
ghdpreto <ghdpreto@gmail.com>
2021-01-21 23:47:20 -0300

    Crdiado o modal para input dos dados 🚧

---
+++ script.js
@@ -0,0 +1,10 @@
+Modal = {
+    open(){
+        const modal = document.querySelector('.modal-overlay')
+        modal.classList.add('active')
+    },
+    close(){
+        const modal = document.querySelector('.modal-overlay')
+        modal.classList.remove('active')        
+    }
+}
