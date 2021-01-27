5133f9a4d99b4ea1f04198fdc0beffa634dc4fa4
ghdpreto <ghdpreto@gmail.com>
2021-01-27 19:59:11 -0300

    Adicionado animações  :lipstick:

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ Modal = {
     open(){
         const modal = document.querySelector('.modal-overlay')
         modal.classList.add('active')
+
+        const modal_box = document.querySelector('.modal')
+        modal_box.classList.add('animate-up')
     },
     close(){
         const modal = document.querySelector('.modal-overlay')
-        modal.classList.remove('active')        
+        modal.classList.remove('active')
+
+        const modal_box = document.querySelector('.modal')
+        modal_box.classList.remove('animate-up')
     }
 }
